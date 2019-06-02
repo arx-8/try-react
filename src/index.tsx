d51d1ffdfdbaa5e05902172027d20cff2416ee1e
arx-8 <swans.randomness@gmail.com>
2019-06-02 14:03:35 +0900

    Install emotion-normalize

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,36 @@ import ReactDOM from "react-dom"
 import "index.css"
 import { App } from "components/pages/App"
 import * as serviceWorker from "./serviceWorker"
+import { Global, css } from "@emotion/core"
+import emotionNormalize from "emotion-normalize"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const globalStyles = css`
+  ${emotionNormalize}
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+      "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+      "Helvetica Neue", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+`
+
+const Root: React.FC = () => {
+  return (
+    <>
+      <Global styles={globalStyles} />
+      <App />
+    </>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
