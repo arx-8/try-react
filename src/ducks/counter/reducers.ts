50a5923dd0dbc5764f435c1fcb534d3f7edd1cd4
arx-8 <swans.randomness@gmail.com>
2019-06-08 19:37:46 +0900

    Install immer

--- src/ducks/counter/reducers.ts
+++ src/ducks/counter/reducers.ts
@@ -1,5 +1,6 @@
 import { handleActions, ReduxCompatibleReducer } from "redux-actions"
 import { CounterActionType } from "./types"
+import produce from "immer"
 
 export type CounterState = {
   count: number
@@ -17,28 +18,24 @@ export const reducer: ReduxCompatibleReducer<
 > = handleActions(
   {
     [CounterActionType.ADD]: (state, action) => {
-      return {
-        ...state,
-        count: state.count + action.payload.count,
-      }
+      return produce(state, (draft) => {
+        draft.count = state.count + action.payload.count
+      })
     },
     [CounterActionType.DECREMENT]: (state) => {
-      return {
-        ...state,
-        count: state.count - 1,
-      }
+      return produce(state, (draft) => {
+        draft.count = state.count - 1
+      })
     },
     [CounterActionType.INCREMENT]: (state) => {
-      return {
-        ...state,
-        count: state.count + 1,
-      }
+      return produce(state, (draft) => {
+        draft.count = state.count + 1
+      })
     },
     [CounterActionType.CHANGE_DEFAULT_AMOUNT]: (state, action) => {
-      return {
-        ...state,
-        defaultAmount: action.payload.defaultAmount,
-      }
+      return produce(state, (draft) => {
+        draft.defaultAmount = action.payload.defaultAmount
+      })
     },
   },
   initialState
