7bc58d56dccfcea82874d9315eebfc2d1d8205e4
arx-8 <swans.randomness@gmail.com>
2019-06-14 00:02:49 +0900

    Connect component

--- src/components/pages/Members.tsx
+++ src/components/pages/Members.tsx
@@ -1,24 +1,40 @@
 /** @jsx jsx */
 import { Card, Header, Image } from "semantic-ui-react"
+import { connect } from "react-redux"
 import { css, jsx } from "@emotion/core"
+import { Dispatch } from "redux"
+import { RootState } from "ducks/store"
 import { Spinner } from "components/atoms/Spinner"
 import { User } from "types/GitHub"
 import React from "react"
+import { gitHubActions } from "ducks/gitHub"
+
+type ReduxStateProps = {
+  isLoading: boolean
+  users: User[]
+}
+
+type ReduxDispatchProps = {
+  fetchMembersStart: (companyName: string) => void
+}
 
 type Props = {
   children?: never
-}
+} & ReduxStateProps &
+  ReduxDispatchProps
 
-export const Members: React.FC<Props> = () => {
-  const isLoading = true
-  const users: User[] = []
-  const companyName = "<会社名>"
+const _Members: React.FC<Props> = ({ fetchMembersStart, isLoading, users }) => {
+  const companyName = "facebook"
+
+  React.useEffect(() => {
+    fetchMembersStart(companyName)
+  }, [fetchMembersStart])
 
   return (
     <div css={root}>
       <Header as="h2">{companyName}の開発メンバー</Header>
       {isLoading ? (
-        <Spinner></Spinner>
+        <Spinner />
       ) : (
         <Card.Group>
           {users.map((u) => (
@@ -28,10 +44,9 @@ export const Members: React.FC<Props> = () => {
               target="_blank"
             >
               <Card.Content>
-                <Image floated="right" size="mini" src={u.avatar_url}>
-                  <Card.Header>{u.login}</Card.Header>
-                  <Card.Meta>GitHub ID: {u.id}</Card.Meta>
-                </Image>
+                <Image floated="right" size="mini" src={u.avatar_url}></Image>
+                <Card.Header>{u.login}</Card.Header>
+                <Card.Meta>GitHub ID: {u.id}</Card.Meta>
               </Card.Content>
             </Card>
           ))}
@@ -44,3 +59,23 @@ export const Members: React.FC<Props> = () => {
 const root = css`
   margin: 2em 1.5em;
 `
+
+const mapStateToProps = (state: RootState): ReduxStateProps => {
+  const { users, isLoading } = state.gitHub
+  return {
+    isLoading,
+    users,
+  }
+}
+
+const mapDispatchToProps = (dispatch: Dispatch): ReduxDispatchProps => {
+  return {
+    fetchMembersStart: (companyName) =>
+      dispatch(gitHubActions.fetchMembersStart({ companyName })),
+  }
+}
+
+export const Members = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(_Members)
