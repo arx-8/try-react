5edfa9616953a844c9e61ad0e74546a6b506118b
arx-8 <swans.randomness@gmail.com>
2019-06-23 00:18:48 +0900

    feat: Add base

--- src/components/helpers/Routes.tsx
+++ src/components/helpers/Routes.tsx
@@ -2,11 +2,12 @@ import { Counter } from "components/pages/Counter"
 import { Members } from "components/pages/Members"
 import { NotFound } from "components/pages/NotFound"
 import { Root } from "components/pages/Root"
-import { Route, Switch } from "react-router-dom"
-import { RoutePath } from "constants/Paths"
 import { TicTacToePage } from "components/pages/TicTacToePage"
+import { TodoAsyncPage } from "components/pages/TodoAsyncPage"
 import { TodoPage } from "components/pages/TodoPage"
+import { RoutePath } from "constants/Paths"
 import React from "react"
+import { Route, Switch } from "react-router-dom"
 
 type Props = {
   children?: never
@@ -18,6 +19,7 @@ export const Routes: React.FC<Props> = () => {
       <Route exact path={RoutePath.Root} component={Root} />
       <Route exact path={RoutePath.Counter} component={Counter} />
       <Route exact path={RoutePath.Todo} component={TodoPage} />
+      <Route exact path={RoutePath.TodoAsync} component={TodoAsyncPage} />
       <Route exact path={RoutePath.TicTacToe} component={TicTacToePage} />
       <Route exact path={RoutePath.GitHubExplorer} component={Members} />
       <Route
---
+++ src/components/pages/TodoAsyncPage/index.tsx
@@ -0,0 +1,36 @@
+import Container from "@material-ui/core/Container"
+import Paper from "@material-ui/core/Paper"
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
+import Typography from "@material-ui/core/Typography"
+import { TodoActions } from "components/organisms/TodoActions/AsyncContainer"
+import { TodoInput } from "components/organisms/TodoInput/AsyncContainer"
+import { TodoList } from "components/organisms/TodoList/AsyncContainer"
+import React from "react"
+
+type Props = {
+  children?: never
+}
+
+export const TodoAsyncPage: React.FC<Props> = () => {
+  const classes = useStyles()
+
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h2">My TODO (Async)</Typography>
+      <Paper className={classes.root}>
+        <TodoInput />
+        <TodoList />
+        <TodoActions />
+      </Paper>
+    </Container>
+  )
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      marginTop: theme.spacing(1),
+      padding: theme.spacing(3, 2),
+    },
+  })
+)
