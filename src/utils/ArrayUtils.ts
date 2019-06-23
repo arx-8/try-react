e751e1568805fe82358d2e1cba23b1f8a8d5839c
arx-8 <swans.randomness@gmail.com>
2019-06-24 00:59:55 +0900

    feat: performance tuning by shouldComponentUpdate

--- src/utils/ArrayUtils.ts
+++ src/utils/ArrayUtils.ts
@@ -9,3 +9,26 @@ export const range = (end: number): number[] => {
 export const toUniq = <T extends number | string>(array: T[]): T[] => {
   return [...new Set(array)]
 }
+
+/**
+ * params array must be sorted.
+ */
+export const equals = <T>(
+  arr1: T[],
+  arr2: T[],
+  predicate: (elm1: T, elm2: T) => boolean
+): boolean => {
+  const arrLen = arr1.length
+  if (arrLen !== arr2.length) {
+    return false
+  }
+
+  for (let index = 0; index < arrLen; index++) {
+    const e1 = arr1[index]
+    const e2 = arr2[index]
+    if (!predicate(e1, e2)) {
+      return false
+    }
+  }
+  return true
+}
