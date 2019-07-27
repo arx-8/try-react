5e4435f607eb801abe50fa7f0dda0721b54b82a1
arx-8 <swans.randomness@gmail.com>
2019-07-28 01:37:31 +0900

    feat: Persist auth state

--- src/types/@typesAlternative.d.ts
+++ src/types/@typesAlternative.d.ts
@@ -3,3 +3,14 @@
  */
 
 declare module "emotion-normalize"
+
+/**
+ * DefinitelyTyped にある定義が 1.x (beta) 用なため、0.x用に上書き
+ */
+declare module "redux-localstorage" {
+  // eslint-disable-next-line import/no-default-export
+  export default function persistState(
+    paths: string[],
+    config?: Record<string, any>
+  ): any
+}
